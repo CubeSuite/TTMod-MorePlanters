e8d291e26f44902942bcd7a3b9d5088a7fa8043b
CubeSuite <cubesuite@outlook.com>
2024-05-18 20:17:12 +0100

    Add project files.

---
+++ MorePlanters/Patches/SaveStatePatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorePlanters.Patches
+{
+    internal class SaveStatePatch
+    {
+        [HarmonyPatch(typeof(SaveState), "SaveToFile")]
+        [HarmonyPostfix]
+        static void SavePlanterExtensions() {
+            MorePlantersPlugin.SaveData(SaveState.instance.metadata.worldName);
+        }
+    }
+}
